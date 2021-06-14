0b08386ee5a2f1692c203eeb7f15141453e7ce40
rimichaeru <conceptual@protonmail.com>
2021-06-14 17:01:04 +0100

    Added second input

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <input type="file" accept="image/*" capture="environment" />
+      <input type="file" accept="image/*" />
     </div>
   );
 }
